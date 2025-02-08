dd2382aa3d2ef5ed0b99129e30478a92f7602110
Zhanna Suianina <cezyby3@gmail.com>
2025-02-08 22:10:38 +0200

    timer correction

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,7 +10,7 @@ const hoursElements = document.querySelector('[data-hours]');
 const minutesElement = document.querySelector('[data-minutes]');
 const secondsElements = document.querySelector('[data-seconds]');
 
-startBtn.disable = true;
+startBtn.disabled = true;
 let userSelectedDate;
 let intervalId;
 
@@ -28,9 +28,9 @@ flatpickr('#datetime-picker', {
         message: 'Please choose a date in the future',
         position: 'topRight',
       });
-      startBtn.disable = true;
+      startBtn.disabled = true;
     } else {
-      startBtn.disable = false;
+      startBtn.disabled = false;
     }
   },
 });
@@ -38,8 +38,8 @@ flatpickr('#datetime-picker', {
 startBtn.addEventListener('click', () => {
   const initTime = userSelectedDate.getTime();
 
-  startBtn.disable = true;
-  dayInput.disable = true;
+  startBtn.disabled = true;
+  dayInput.disabled = true;
 
   intervalId = setInterval(() => {
     const currentTime = Date.now();
@@ -47,37 +47,35 @@ startBtn.addEventListener('click', () => {
 
     if (remainingTime <= 0) {
       clearInterval(intervalId);
-      startBtn.disable = false;
-      dayInput.disable = false;
+      iziToast.success({
+        message: 'Countdown finished!',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+      dayInput.disabled = false;
       return;
     }
-    
 
     const time = convertMs(remainingTime);
-    updateTimerDisplay(time);  }, 1000);
+    updateTimerDisplay(time);
+  }, 1000);
 });
 
-
 function convertMs(ms) {
- 
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-
   const days = Math.floor(ms / day);
-
   const hours = Math.floor((ms % day) / hour);
- 
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-function updateTimerDisplay({days, hours, minutes, seconds}){
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
   daysElement.textContent = formatTimeComponent(days);
   hoursElements.textContent = formatTimeComponent(hours);
   minutesElement.textContent = formatTimeComponent(minutes);
