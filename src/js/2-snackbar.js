89d8a98bb075c8fb594aec7d0978c471edb9011b
Zhanna Suianina <cezyby3@gmail.com>
2025-02-09 08:27:22 +0200

    snackbar.css

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,40 +5,52 @@ const form = document.querySelector(".form");
 const delayInput = document.querySelector(".delay-input");
 const stateInputs = document.querySelectorAll('input[name="state"]');
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 
-  const delay = parseInt(delayInput.value, 10);
-  const state = Array.from(stateInputs).find(input => input.checked).value;
-
-  createPromise(delay, state)
-    .then(() => {
-      iziToast.success({
-        title: 'Fulfilled',
-        message: `✅ Fulfilled promise in ${delay}ms`,
-        position: 'topRight',
-      });
-    })
-    .catch(() => {
-      iziToast.error({
-        title: 'Rejected',
-        message: `❌ Rejected promise in ${delay}ms`,
-        position: 'topRight',
-      });
+function createPromise(value, delay, status){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() =>{
+            if(status) resolve(value);
+            else reject(value);
+        }, delay)
     });
-
-  delayInput.value = "";
-  stateInputs.forEach(input => input.checked = false);
-});
-
-function createPromise(delay, state) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === "fulfilled") {
-        resolve();
-      } else {
-        reject();
-      }
-    }, delay);
-  });
+    return promise;
 }
+
+function onFulfilled(value, delay){
+    iziToast.show({
+        message: `✅ ${value} promise in ${delay}ms`,
+        backgroundColor: 'rgba(89, 161, 13, 0.8)',
+        messageColor: 'rgb(255, 255, 255)',
+        position: 'topRight',
+    });
+}
+
+function onRejected(value, delay) {
+    iziToast.show({
+      message: `❌ ${value} promise in ${delay}ms`,
+      backgroundColor: 'rgba(239, 64, 64, 0.8)',
+      messageColor: 'rgb(255, 255, 255)',
+      position: 'topRight',
+    });
+  }
+
+
+  form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    const delay = parseInt(delayInput.value, 10);
+    const value = Array.from(stateInputs).find(input => input.checked).value;
+    const status = value === "fulfilled";
+
+    createPromise(value, delay, status)
+    .then(() => onFulfilled(value, delay))
+    .catch(() => onRejected(value, delay));
+
+    delayInput.value = "";
+
+    stateInputs.forEach( input =>{
+        input.checked = false;
+    });
+  });
+
+
