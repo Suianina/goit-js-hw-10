a1eb0f2c522f8638dce25a7f9ee232ce2dedcfc7
Zhanna Suianina <cezyby3@gmail.com>
2025-02-09 09:21:53 +0200

    save

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,52 +5,47 @@ const form = document.querySelector(".form");
 const delayInput = document.querySelector(".delay-input");
 const stateInputs = document.querySelectorAll('input[name="state"]');
 
-
-function createPromise(value, delay, status){
-    return new Promise((resolve, reject)=>{
-        setTimeout(() =>{
-            if(status) resolve(value);
+function createPromise(value, delay, status) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (status) resolve(value);
             else reject(value);
-        }, delay)
+        }, delay);
     });
-    return promise;
 }
 
-function onFulfilled(value, delay){
+function onFulfilled(value, delay) {
     iziToast.show({
         message: `✅ ${value} promise in ${delay}ms`,
         backgroundColor: 'rgba(89, 161, 13, 0.8)',
-        messageColor: 'rgb(255, 255, 255)',
+        messageColor: '#ffffff',
         position: 'topRight',
     });
 }
 
 function onRejected(value, delay) {
     iziToast.show({
-      message: `❌ ${value} promise in ${delay}ms`,
-      backgroundColor: 'rgba(239, 64, 64, 0.8)',
-      messageColor: 'rgb(255, 255, 255)',
-      position: 'topRight',
+        message: `❌ ${value} promise in ${delay}ms`,
+        backgroundColor: 'rgba(239, 64, 64, 0.8)',
+        messageColor: '#ffffff',
+        position: 'topRight',
     });
-  }
+}
 
-
-  form.addEventListener("submit", (event) =>{
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const delay = parseInt(delayInput.value, 10);
-    const value = Array.from(stateInputs).find(input => input.checked).value;
+    const state = Array.from(stateInputs).find(input => input.checked);
+    
+    if (!state) return;
+    const value = state.value;
     const status = value === "fulfilled";
 
     createPromise(value, delay, status)
-    .then(() => onFulfilled(value, delay))
-    .catch(() => onRejected(value, delay));
+        .then(() => onFulfilled(value, delay))
+        .catch(() => onRejected(value, delay));
 
-    delayInput.value = "";
-
-    stateInputs.forEach( input =>{
-        input.checked = false;
-    });
-  });
-
+    form.reset();
+});
 
